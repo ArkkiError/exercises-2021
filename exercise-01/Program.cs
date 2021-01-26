c581438d599b548ba6b7030c9788a448de9c682b
Pekka Vassinen <pekka.vassinen@student.samk.fi>
2021-01-26 10:35:43 +0200

    first commit

---
+++ exercise-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pekka";
+            Console.WriteLine(name);
+
+            int age = 33;
+            Console.WriteLine(age);
+
+            double nuMBR = 3.14;
+            Console.WriteLine(nuMBR);
+
+            FunctionExecuted();
+            int multiplied = Multiplied(16);
+            Console.WriteLine(multiplied);
+        }
+        static void FunctionExecuted()
+        {
+            Console.WriteLine("Function Executed!");
+        }
+        static int Multiplied(int b)
+        {
+            return b * 2;
+        }
+    }
+}
